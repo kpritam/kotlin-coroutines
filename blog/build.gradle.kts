17f6f6cf16d1b88ad426fa8e3584247c686da69b
kpritam <kpritam@thoughtworks.com>
2019-10-28 12:08:09 +0530

    runBlocking example in test

--- blog/build.gradle.kts
+++ blog/build.gradle.kts
@@ -3,5 +3,4 @@ val KotlinxVersion = "1.3.2"
 dependencies {
     compile("org.jetbrains.kotlinx", "kotlinx-coroutines-test", KotlinxVersion)
     compile("io.kotlintest", "kotlintest-core", "3.4.2")
-
 }
---
+++ blog/src/main/kotlin/blog/scopes/RunBlockingTestExample.kt
@@ -0,0 +1,20 @@
+package blog.scopes
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    var counter = 0
+    suspend fun incrment() {
+        delay(10)
+        counter += 1
+    }
+
+    // @Test
+    fun `should able to increment counter`() = runBlocking {
+        repeat(50) { incrment() }
+        assert(counter == 50) { "Assertion failed, expected=100 but actual=$counter" }
+    }
+
+    `should able to increment counter`()
+}
